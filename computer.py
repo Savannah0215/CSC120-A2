10716b28bdb43878e26cfde35f55d2fe9465ae23
Savannah0215 <sko65@smith.edu>
2025-05-05 17:33:22 -0400

    Update computer.py

--- computer.py
+++ computer.py
@@ -1,10 +1,17 @@
 class Computer:
-
+    
     # What attributes will it need?
-
+    price: float
+    os: int
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, os:int, amt:float):
+        self.os = os
+        self.price = amt
 
+    def priceUpdate(self, new_price.float):
+        self.price = new_price
+
+    def osUpdate(self, new_os.int):
+        self.os = new_os
     # What methods will you need?
