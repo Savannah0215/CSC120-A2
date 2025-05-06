632a224db4200797b7569d098b86340684228c06
Savannah0215 <sko65@smith.edu>
2025-05-06 12:51:17 -0400

    Update computer.py

--- computer.py
+++ computer.py
@@ -2,16 +2,19 @@ class Computer:
     
     # What attributes will it need?
     price: float
-    os: int
+    os: str
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__(self, os:int, amt:float):
+    def __init__(self, os:str, amt:float):
         self.os = os
         self.price = amt
 
     def priceUpdate(self, new_price.float):
         self.price = new_price
 
-    def osUpdate(self, new_os.int):
+    def osUpdate(self, new_os.str):
         self.os = new_os
+
+    def retrieveComputerInformation(self, price:float, os:str):
+        print("This computer has an operating system of", os, ".\nIts current retail price is", price)
     # What methods will you need?
